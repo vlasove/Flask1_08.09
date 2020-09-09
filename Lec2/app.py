10c77bfa7fce37af9ee54f9b8836b0a30b9bc8d8
Evgen <evgeny_vlasov@yahoo.com>
2020-09-09 19:59:40 +0300

    added app.py

--- Lec2/app.py
+++ Lec2/app.py
@@ -16,8 +16,8 @@
 #     "price" : 14.99
 # }
 
-from flask import Flask 
-from flask_restful import Resource, Api 
+from flask import Flask , request
+from flask_restful import Resource, Api , reqparse
 
 # Общая конфигурация приложения Flask
 app = Flask(__name__)
@@ -31,7 +31,46 @@ items = []
 
 # Ресурс Item /item/<string:name>
 class Item(Resource):
-    pass 
+    # Инициализируем парсер тел запросов
+    parser = reqparse.RequestParser()
+    # Добавляем первый аргумент для проверки
+    parser.add_argument('price', type=float, required=True, help="This field cannot be blank")
+
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item:
+            return {'item' : item}, 200
+        return {'Error': "Item with that name not found"}, 404
+
+    def post(self, name):
+        # Проверим, что товара с именем name еще нет в БД
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'Error' : 'Item with name {} exists'.format(name)}, 400
+        # Получаем тело запроса
+        request_body = Item.parser.parse_args()
+        item = {'name' : name, 'price' : request_body['price']}
+        items.append(item)
+        return item, 201
+
+    def put(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item:
+            # Обновляем его 
+            data = Item.parser.parse_args()
+            item.update(data)
+            return item, 202 
+        return {"Error" : "Item with name {} not found".format(name)}, 404
+
+
+    def delete(self, name):
+        global items 
+        start_len = len(items)
+        items =  list(filter(lambda x: x['name'] != name, items))
+        if abs(len(items) - start_len) > 0:
+            return {'Message' : 'Item deleted'}, 202
+        return {'Error' : 'Item with that name not found'}, 404
+
+
 
 # Ресурс Items /items
 class ItemCollection(Resource):
