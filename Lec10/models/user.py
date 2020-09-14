1d7ca6b275596b395512c3f06590a558f154c2ee
Evgen <evgeny_vlasov@yahoo.com>
2020-09-14 20:07:22 +0300

    added User via ORM

--- Lec10/models/user.py
+++ Lec10/models/user.py
@@ -1,9 +1,19 @@
-import sqlite3
+from db import db 
+
+"""
+Реализовать методы UserModel через SQLAlchemy.
+Аналогично как для ItemModel
+"""
+
 
 # Модель пользователя
-class UserModel:
-    #Внушний атрибут класса , указывающий на имя таблицы
+class UserModel(db.Model):
+    #Внутренний атрибут класса , указывающий на имя таблицы
     __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True) # Вводим новое поле
+    username = db.Column(db.String(50))
+    password =  db.Column(db.String(50))
 
     def __init__(self, _id, username, password):
         self.id = _id 
@@ -12,47 +22,15 @@ class UserModel:
 
 
     def insert(self):
-        conn = sqlite3.connect('data.db')
-        cur = conn.cursor()
-
-        insert_query = "INSERT INTO {} VALUES(NULL, ? ,?)".format(self.__tablename__) 
-        cur.execute(insert_query, (self.username, self.password))
-
-        conn.commit()
-        conn.close()
+        db.session.add(self)
+        db.session.commit()
     
     @classmethod
     def search_username(cls, username):
-        conn = sqlite3.connect('data.db')
-        cur = conn.cursor()
-
-        select_query = "SELECT * FROM {} WHERE username=?".format(cls.__tablename__)
-        sql_row = cur.execute(select_query, (username, )).fetchone() # (1, "Bob", "asp[daspd")
-
-        if sql_row:
-            user = cls(sql_row[0], sql_row[1], sql_row[2])
-        else:
-            user = None 
-
-        conn.close()
-        return user
-
-
+        return cls.query.filter_by(username=username).first()
 
 
     @classmethod
     def search_id(cls, _id) :
-        conn = sqlite3.connect('data.db')
-        cur = conn.cursor()
-
-        select_query = "SELECT * FROM {} WHERE id=?".format(cls.__tablename__)
-        sql_row = cur.execute(select_query, (_id, )).fetchone() # [(1, "Bob", "asp[daspd")]
-
-        if sql_row:
-            user = cls(sql_row[0], sql_row[1], sql_row[2])
-        else:
-            user = None 
-
-        conn.close()
-        return user
+        return cls.query.filter_by(id= _id)
 
